2d8487ee4156b1c09eb10ed252e2e7eba8719b19
AndriiPolishko <ramp8a799@gmail.com>
2023-11-15 10:43:50 +0200

    add missing files

---
+++ portable_types/portable_thread.h
@@ -0,0 +1,81 @@
+#ifndef MY_THREAD_PORTABLE_THREAD_H
+#define MY_THREAD_PORTABLE_THREAD_H
+
+#include <tuple>
+
+#ifdef __linux
+	#include <pthread.h>
+	typedef void* thread_routine;
+	typedef void* mvoid;
+#elif defined WIN32
+	#include <windows.h>
+	typedef DWORD thread_routine;
+	typedef LPVOID mvoid;
+#endif //WIN32
+
+template <typename Callable, typename... Args>
+struct ThreadData
+{
+	Callable func;
+	std::tuple<Args...> args;
+
+	void invoke()
+	{
+		std::apply(func, args);
+	}
+};
+
+template <typename Callable, typename... Args>
+static thread_routine thread_start_routine(mvoid arg)
+{
+	auto* threadData = static_cast<ThreadData<Callable, Args...>*>(arg);
+	threadData->invoke();
+	delete threadData;
+	return 0;
+}
+
+#ifdef __linux
+	typedef pthread_t my_thread_t;
+	typedef void** retval;
+
+	template <typename Callable, typename... Args>
+	my_thread_t thread_create(my_thread_t* handle, Callable&& func, Args&&... args)
+	{
+		auto threadData = new ThreadData<Callable, Args...>{std::forward<Callable>(func), std::forward_as_tuple(args...)};
+
+		pthread_create(handle, NULL, thread_start_routine<Callable, Args...>, threadData);
+		return *handle;
+	}
+
+	static void m_join(my_thread_t my_thread, retval val)
+	{
+		pthread_join(my_thread, val);
+	}
+	static void thread_destroy(my_thread_t my_thread)
+	{
+		//just a stub
+	}
+#elif defined WIN32
+	typedef HANDLE my_thread_t;
+	typedef DWORD* retval;
+
+	template <typename Callable, typename... Args>
+	my_thread_t thread_create(my_thread_t handle, Callable&& func, Args&&... args)
+	{
+		auto threadData = new ThreadData<Callable, Args...>{std::forward<Callable>(func), std::forward_as_tuple(args...)};
+
+		return CreateThread(nullptr, 0, thread_start_routine<Callable, Args...>, threadData, 0, 0);
+	}
+
+	static void m_join(my_thread_t my_thread, retval val)
+	{
+		WaitForSingleObject(my_thread, INFINITE);
+
+	}
+	static void thread_destroy(my_thread_t my_thread)
+	{
+		CloseHandle(my_thread);
+	}
+#endif // __linux
+
+#endif //MY_THREAD_PORTABLE_THREAD_H
